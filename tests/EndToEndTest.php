ed35afea96ea84a79cae3023548ba0157ec988c8
Jesse Kramer <jesse@kramerventures.nl>
2020-05-18 11:35:07 +0200

    Update testdata to example.com

--- tests/EndToEndTest.php
+++ tests/EndToEndTest.php
@@ -13,43 +13,43 @@ class EndToEndTest extends TestCase
     public function test_mx_records()
     {
         $dig = new MockHandler();
-        $dig->setMockResponse('dutch.fitness.		3600 IN	MX 10 primary.mail.pcextreme.nl.
-dutch.fitness.		3600 IN	MX 20 fallback.mail.pcextreme.nl.
+        $dig->setMockResponse('example.com.		3600 IN	MX 10 primary.mail.example.com.
+example.com.		3600 IN	MX 20 fallback.mail.example.com.
 ');
         $service = new DnsResolver($dig);
-        $records = $service->resolve('dutch.fitness', 'MX');
+        $records = $service->resolve('example.com', 'MX');
         $this->assertInstanceOf(RecordSet::class, $records);
         $this->assertSame(2, $records->count());
 
         $this->assertInstanceOf(Record::class, $records[0]);
-        $this->assertSame('dutch.fitness', $records[0]->getName());
+        $this->assertSame('example.com', $records[0]->getName());
         $this->assertSame(3600, $records[0]->getTTL());
         $this->assertSame('IN', $records[0]->getClass());
         $this->assertSame('MX', $records[0]->getType());
         $this->assertSame(10, $records[0]->getPrio());
-        $this->assertSame('primary.mail.pcextreme.nl', $records[0]->getContent());
+        $this->assertSame('primary.mail.example.com', $records[0]->getContent());
 
         $this->assertInstanceOf(Record::class, $records[1]);
-        $this->assertSame('dutch.fitness', $records[1]->getName());
+        $this->assertSame('example.com', $records[1]->getName());
         $this->assertSame(3600, $records[1]->getTTL());
         $this->assertSame('IN', $records[1]->getClass());
         $this->assertSame('MX', $records[1]->getType());
         $this->assertSame(20, $records[1]->getPrio());
-        $this->assertSame('fallback.mail.pcextreme.nl', $records[1]->getContent());
+        $this->assertSame('fallback.mail.example.com', $records[1]->getContent());
     }
 
     public function test_a_records()
     {
         $dig = new MockHandler();
-        $dig->setMockResponse('dutch.fitness.		3600	IN	A	104.198.14.52.
+        $dig->setMockResponse('example.com.		3600	IN	A	104.198.14.52.
 ');
         $service = new DnsResolver($dig);
-        $records = $service->resolve('dutch.fitness', 'A');
+        $records = $service->resolve('example.com', 'A');
         $this->assertInstanceOf(RecordSet::class, $records);
         $this->assertSame(1, $records->count());
 
         $this->assertInstanceOf(Record::class, $records[0]);
-        $this->assertSame('dutch.fitness', $records[0]->getName());
+        $this->assertSame('example.com', $records[0]->getName());
         $this->assertSame(3600, $records[0]->getTTL());
         $this->assertSame('IN', $records[0]->getClass());
         $this->assertSame('A', $records[0]->getType());
